4b8d2e6a6272249c2fec17d633f2fc76596da3a3
AbyssBagel <geille.antonin@gmail.com>
2024-11-14 10:15:04 -0500

    update username logic

--- app.py
+++ app.py
@@ -21,7 +21,6 @@ def handle_connect():
   users[request.sid] = {"username": username, "private_key": private_key}
 
   emit("connected", {"username": username}, broadcast=True)
-
   emit("set_username", {"username": username})
 
 # This function will be called when the client disconnects from the server
@@ -37,6 +36,15 @@ def handle_message(data):
   user = users.get(request.sid, None)
   if user:
     emit("new_message", {"username": user["username"], "message": data["message"]}, broadcast=True)
+
+# This function will be called when the client updates the username
+@socketio.on("update_username")
+def handle_message(data):
+  user = users.get(request.sid, None)
+  if user:
+    old_username = users[request.sid]["username"]
+    user[request.sid]["username"] = data["username"]
+    emit("update_username", {"old_username": old_username, "new_username": data["username"]}, broadcast=True)
 
 if __name__ == '__main__':
   socketio.run(app)
